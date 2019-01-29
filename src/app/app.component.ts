c3ca982a53392f0181d3daf37d8d363122c7b382
Nguyen Van Thong (FHN.DSL) <ThongNV1@fsoft.com.vn>
2019-01-29 13:46:52 +0700

    fix logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
-import './web-components/radio-btn-group.wc';
 import './web-components/radio-btn-multiple.wc';
 
 @Component({
@@ -11,76 +8,39 @@ import './web-components/radio-btn-multiple.wc';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  isLoading: boolean;
-  moods: Array<string> = [
-    'awesome',
-    'formidable',
-    'great',
-    'terrifying',
-    'wonderful',
-    'astonishing',
-    'breathtaking'
-  ];
-  radios: Array<object> = [
-    {
-      name: 'request',
-      value: 'Famille postale',
-      selected: false,
-      children: [
-        {
-          name: 'famille',
-          value: 'Compte',
-          selected: false,
-        }
-      ]
-    },
-    {
-      name: 'request',
-      value: 'Compte',
-      selected: true,
-      children: [
-        {
-          name: 'postal',
-          value: 'Produit postal',
-          selected: true,
-        },
-        {
-          name: 'postal',
-          value: 'Famille postale',
-          selected: false,
-        }
-      ]
-    }
-  ];
-  private mood: string;
-  isChanged = false;
-  constructor(
-    private router: Router,
-  ) {
-    this.randomMood();
-  }
-
-  randomMood() {
-    const index = Math.floor(Math.random() * this.moods.length);
-    if (this.mood === this.moods[index]) {
-      return this.randomMood();
-    }
-    this.mood = this.moods[index];
-  }
-
-  moodChanged(e: CustomEvent) {
-    console.log(e.detail.selectedRadio);
-  }
+  radios: Array<object>;
 
   ngOnInit(): void {
-    // We can get the language to set for application
-    // this.translate.use('currentLang');
-    // this.titleService.setTitle(environment.pageTitle);
-    // this.loadingScreenServices.Loading().subscribe((data) => {
-    //   setTimeout(() => {
-    //     this.isLoading = data;
-    //   }, 0);
-    // });
+    this.radios = [
+      {
+        name: 'request',
+        value: 'Famille postale',
+        selected: false,
+        children: [
+          {
+            name: 'famille',
+            value: 'Compte',
+            selected: false,
+          }
+        ]
+      },
+      {
+        name: 'request',
+        value: 'Compte',
+        selected: true,
+        children: [
+          {
+            name: 'postal',
+            value: 'Produit postal',
+            selected: true,
+          },
+          {
+            name: 'postal',
+            value: 'Famille postale',
+            selected: false,
+          }
+        ]
+      }
+    ];
   }
 }
-
--- src/app/web-components/radio-btn-multiple.wc.js
+++ src/app/web-components/radio-btn-multiple.wc.js
@@ -1,15 +1,11 @@
 import { LitElement, html } from "lit-element";
+
 import { style } from "./radio-btn-group.wc-styles";
 
 class WebComponentsRadioBtnMultiple extends LitElement {
   static get properties() {
     return {
-      name: String,
-      values: Array,
       radios: Array,
-      selectedRadio: Object,
-      selectedValue: String,
-      valueLength: Number = 0
     };
   }
 
@@ -26,38 +22,34 @@ class WebComponentsRadioBtnMultiple extends LitElement {
               <div class="radio-box">
                 <label class="radio-button">
                   ${radio.value}
-                  <input type="radio" name="${radio.name}" checked="${radio.selected}" @click="${() => this.select(radio)}"/>
+                  <input type="radio" name="${radio.name}" .checked="${radio.selected}" @click="${() => this.select(radio)}"/>
                   <span class="checkmark"></span>
                 </label>
                 <div class="radio-group" .hidden=${!radio.selected}>
-                ${radio.children.map(
-                  (child, index) => html`
-                    <div class="radio-box-child">
-                      <div class="radio-box">
-                        <label class="radio-button">
-                          ${child.value}
-                          <input type="radio" name="${child.name}" .checked=${child.selected} />
-                          <span class="checkmark"></span>
-                          ${!index ? html`<div class="addition">- et -</div>` : ''}
-                        </label>
-                      </div>
-                    </div>`
-                  )
-                }
+                  ${radio.children.map(
+                    (child, index) => html`
+                      <div class="radio-box-child">
+                        <div class="radio-box">
+                          <label class="radio-button">
+                            ${child.value} 
+                            <input type="radio" name="${child.name}" .checked="${child.selected}" @click="${() => this.selectChild(radio, child)}"/>
+                            <span class="checkmark"></span>
+                            ${!index ? html`<div class="addition">- et -</div>` : ''}
+                          </label>
+                        </div>
+                      </div>`
+                    )
+                  }
                 </div>
               </div>
             `
           )
         }
       </div>
-      ${this.selectedValue}
     `;
   }
 
   select(radio) {
-    // this.selectedRadio = radio;
-    // radio.selected = false;
-    // radio.children.map(child => child.selected = false);
     this.radios = this.radios.map(r => {
       r.selected = r.value === radio.value;
       r.children.map(child => {
@@ -65,30 +57,31 @@ class WebComponentsRadioBtnMultiple extends LitElement {
         return child;
       });
       return r;
-    })
-    // this.requestUpdate();
+    });
+    this._emitSelectedRadio(radio);
+  }
+
+  selectChild(radio, child) {
+    this.radios = this.radios.map(r => {
+      r.children.map(c => {
+        c.selected = r.value === radio.value && c.value === child.value;
+        return c;
+      });
+      return r;
+    });
+    this._emitSelectedRadio(radio);
+  }
+
+  _emitSelectedRadio(radio) {
     this.dispatchEvent(
       new CustomEvent("clicked", {
         detail: {
-          selectedRadio: this.selectedRadio
+          selectedRadio: radio
         }
       })
     );
-    // console.log(radio.value);
   }
 
-  // clicked(value) {
-  //   // this.selectedValue = value;
-  //   this.requestUpdate();
-  //   this.dispatchEvent(
-  //     new CustomEvent("clicked", {
-  //       detail: {
-  //         selectedValue: this.selectedValue
-  //       }
-  //     })
-  //   );
-  //   console.log(this.values);
-  // }
 }
 
 customElements.define("wc-radio-btn-multiple", WebComponentsRadioBtnMultiple);
